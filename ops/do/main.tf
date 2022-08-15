c8c820892f8ad812a189711a3a00b8889fe90dd0
Sam Foster <sfoster.ka@gmail.com>
2022-08-15 13:45:06 -0700

    Add domain setup

--- ops/do/main.tf
+++ ops/do/main.tf
@@ -6,27 +6,39 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+
 data "digitalocean_ssh_key" "terraform" {
   name = "Ueno-ssh-0"
 }
+
+resource "digitalocean_domain" "samtest" {
+  name = "sfoster3.com"
+}
+
 
 resource "digitalocean_app" "iw1" {
   spec {
     name   = "iw1"
     region = "sfo"
 
+    domain {
+      name = digitalocean_domain.samtest.name
+      type = "PRIMARY"
+      zone = digitalocean_domain.samtest.name
+    }
+
     static_site {
 
       github {
-        repo = "xyrider0/IllustrationWebsite"
+        repo           = "xyrider0/IllustrationWebsite"
         branch         = "main"
         deploy_on_push = false
       }
 
-      name          = "iw1"
-      build_command = "npm ci; npm run build"
-      source_dir    = "/web_pkae2"
-      output_dir = "/build"
+      name              = "iw1"
+      build_command     = "npm ci; npm run build"
+      source_dir        = "/web_pkae2"
+      output_dir        = "/build"
       catchall_document = "index.html"
     }
   }
@@ -35,5 +47,5 @@ resource "digitalocean_app" "iw1" {
 resource "digitalocean_project" "iw1" {
   name        = "Illustration Website v1"
   environment = "Development"
-  resources   = ["do:app:${digitalocean_app.iw1.id}"]
+  resources   = ["do:app:${digitalocean_app.iw1.id}", digitalocean_domain.samtest.urn]
 }
