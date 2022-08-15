77f2520c4acd3d8e75ad40b6ce24a9d5856a549c
Sam Foster <sfoster.ka@gmail.com>
2022-08-15 12:57:30 -0700

    Add do tf setup

---
+++ ops/do/main.tf
@@ -0,0 +1,39 @@
+variable "do_token" {}
+variable "pvt_key" {}
+
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_ssh_key" "terraform" {
+  name = "Ueno-ssh-0"
+}
+
+resource "digitalocean_app" "iw1" {
+  spec {
+    name   = "iw1"
+    region = "sfo"
+
+    static_site {
+
+      github {
+        repo = "xyrider0/IllustrationWebsite"
+        branch         = "main"
+        deploy_on_push = false
+      }
+
+      name          = "iw1"
+      build_command = "npm ci; npm run build"
+      source_dir    = "/web_pkae2"
+      output_dir = "/build"
+      catchall_document = "index.html"
+    }
+  }
+}
+
+resource "digitalocean_project" "iw1" {
+  name        = "Illustration Website v1"
+  environment = "Development"
+  resources   = ["do:app:${digitalocean_app.iw1.id}"]
+}
