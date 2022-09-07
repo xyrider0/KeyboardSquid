f92498a3555f138e3643fd64b6ea37bfe7f2cafb
Eric Tu <speaktopkae2@gmail.com>
2022-09-07 13:07:14 -0700

    Added Footer

---
+++ web_pkae2/src/components/Footer/Footer.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import PixivIcon from "./PixivIcon";
+import { SocialIcon } from 'react-social-icons';
+
+export default function Footer() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <MDBCol md="7" lg="7" xl="7" className='mx-auto'>
+              <h6 className='text-uppercase fw-bold mb-4' align="center">
+                Socials
+              </h6>
+              <p align="center">
+                <div>
+                  <a href='https://twitch.tv/pkae_2' className='me-4 text-reset'>
+                    <i class="fab fa-twitch"></i>
+                  </a>
+                  <a href='https://twitter.com/2_pkae' className='me-4 text-reset'>
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                  <a href='https://www.pixiv.net/en/users/10313151' className='me-4 text-reset'>
+                    <PixivIcon></PixivIcon>
+                  </a>
+                  <a href='https://www.instagram.com/pkae_2/' className='me-4 text-reset'>
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                  <a href='' className='me-4 text-reset'>
+                    <SocialIcon url="https://www.tiktok.com/@pkae_2" bgColor="#6c757d" style={{ top: -3, height: 17, width: 17 }}/>
+                  </a>
+                  <a href='' className='me-4 text-reset'>
+                    <i class="fab fa-discord"></i>
+                  </a>
+                </div>
+              </p>
+            </MDBCol>
+      </section>
+
+      <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md="5" lg="5" xl="5" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                Pkae2 Illustrations
+              </h6>
+              <p>
+                
+              </p>
+            </MDBCol>
+
+            <MDBCol md="5" lg="5" xl="5" className='mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              {/* <p>
+                <MDBIcon icon="home" className="me-2" />
+                CA, US
+              </p> */}
+              <p>
+                <MDBIcon icon="envelope" className="me-3" />
+                speaktopkae2@gmail.com
+              </p>
+              {/* <p>
+                <MDBIcon icon="phone" className="me-3" /> 
+              </p>
+              <p>
+                <MDBIcon icon="print" className="me-3" /> 
+              </p> */}
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+      </div>
+    </MDBFooter>
+  );
+}
---
+++ web_pkae2/src/components/Footer/PixivIcon.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function PixivIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 30 240 350"
+      height = "20px"
+      width = "20px"
+      top = "-2px"
+      version="1.1"
+    >
+      <g transform="translate(0 -752.362)">
+        <path
+          fill="#6c757d"
+          fillOpacity="1"
+          stroke="#6c757d"
+          strokeDasharray="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="4"
+          strokeOpacity="1"
+          strokeWidth="5"
+          d="M162.612 779.085c-42.567.793-93.967 20.35-150.875 69.781l15.156 32.844 8.25-4.781V859.99c4.895-9.83 11.734-18.87 25.156-25.187v189.406H53.33l.063 6.75h47.25v-6.563h-7.094v-47.75c263.738 59.097 228.292-200.528 69.063-197.562zm-1.97 18.25c127.518.894 130.47 225.42-67.093 158.094V814.522c25.1-12.205 47.564-17.324 67.094-17.187z"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default PixivIcon;
