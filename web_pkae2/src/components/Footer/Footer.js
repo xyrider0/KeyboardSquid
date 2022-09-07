1eb9c3e2c703cc75e1ef88fc99681d901f60e890
Eric Tu <speaktopkae2@gmail.com>
2022-09-07 14:08:06 -0700

    Add discord hyperlink

--- web_pkae2/src/components/Footer/Footer.js
+++ web_pkae2/src/components/Footer/Footer.js
@@ -28,7 +28,7 @@ export default function Footer() {
                   <a href='' className='me-4 text-reset'>
                     <SocialIcon url="https://www.tiktok.com/@pkae_2" bgColor="#6c757d" style={{ top: -3, height: 17, width: 17 }}/>
                   </a>
-                  <a href='' className='me-4 text-reset'>
+                  <a href='https://discord.gg/dgHa5Ttj' className='me-4 text-reset'>
                     <i class="fab fa-discord"></i>
                   </a>
                 </div>
