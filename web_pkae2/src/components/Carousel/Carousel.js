84f75403509916ac68feaf649dbb22aa5785d2ff
Eric Tu <speaktopkae2@gmail.com>
2022-09-09 13:08:28 -0700

    Updated Commissions Banner Image

--- web_pkae2/src/components/Carousel/Carousel.js
+++ web_pkae2/src/components/Carousel/Carousel.js
@@ -7,39 +7,34 @@ import {
 import yoimiya from './yoimiya.png';
 import koba from './koba.png';
 import cecilia from './cecilia.png';
-import './Carousel.css';
+import commissions from './CommissionsBanner.png';
 // Standard size for png should be  3850x2525 pixels
 
 
 export default function Carousel() {
   return (
       <MDBContainer className="mb-3 mt-3">
-          <MDBCarousel showControls showIndicators className="col-lg-8 offset-lg-2 col-md-12">
-            <MDBCarouselItem
-              className='w-100 d-block'
+          <MDBCarousel showControls showIndicators className="col-lg-8 offset-lg-2 col-md-12 col-sm-10">
+          <MDBCarouselItem
+              className='w-100 d-sm-block'
               itemId={1}
               src={koba}
               alt='...'
             >
-              <h1>
-                Gallery <br/> (WIP)
-              </h1>
             </MDBCarouselItem>
             <MDBCarouselItem
-              className='w-100 d-block dark'
+              className='w-100 d-sm-block'
               itemId={2}
-              src={yoimiya}
+              src={commissions}
               alt='...'
             >
-              <h1> Commissions <br/> (WIP)</h1>
             </MDBCarouselItem>
             <MDBCarouselItem
-              className='w-100 d-block'
+              className='w-100 d-sm-block'
               itemId={3}
               src={cecilia}
               alt='...'
             >
-              <h1> Merch <br/> (WIP)</h1>
             </MDBCarouselItem>
           </MDBCarousel>
       </MDBContainer>
