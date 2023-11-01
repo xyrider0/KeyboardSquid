e4f3b957fb1ffc05174427311aac9c7a6035d959
Owner <erictu9@gmail.com>
2023-10-31 20:26:26 -0700

    Added Home Page Styling and materials

--- web_pkae2/src/components/Carousel/Carousel.js
+++ web_pkae2/src/components/Carousel/Carousel.js
@@ -1,31 +1,22 @@
 import React from 'react';
-import {
-  MDBCarousel,
-  MDBCarouselItem,
-  MDBContainer,
-} from 'mdb-react-ui-kit';
 import {Link} from 'react-router-dom';
-import merch from './MerchandiseBanner.png';
-import commissions from './CommissionsBanner.png';
-import gallery from './GalleryBanner.png';
 // Standard size for png should be  3850x2525 pixels
 
 
 export default function Carousel() {
   return (
-      <MDBContainer className="mb-3 mt-3">
-          <MDBCarousel showControls showIndicators className="col-m-8 offset-m-2">
-          <Link to='gallery'>
-            <MDBCarouselItem
-              className='w-100 d-sm-block'
-              itemId={1}
-              src={gallery}
-              alt='...'
-            >
-            </MDBCarouselItem>
-          </Link>
+    <div id="carouselExampleCaptions" className="carousel slide carousel-fade" data-mdb-ride="carousel">
+      <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/8-col/img%282%29.jpg" className="d-block w-100" alt="Wild Landscape"/>
+      <div className="mask" style={{"backgroundColor": "rgba(0, 0, 0, 0.4)"}}></div>
+        <div className="carousel-caption d-none d-sm-block mb-5">
+            <h1 className="mb-4">
+                    <strong>One Stop for Custom Keyboard Design Solutions</strong>
+                </h1>
 
-          </MDBCarousel>
-      </MDBContainer>
+                <Link to="/Merch" className="btn btn-outline-white btn-lg">
+                    Upload Your Design Now!
+                </Link>
+        </div>
+    </div>
   );
 }
